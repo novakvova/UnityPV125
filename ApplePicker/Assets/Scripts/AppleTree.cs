8a7f52ae797da423c6066359ba243d65fa0ccce7
novakvova <novakvova@gmail>
2023-11-13 20:47:02 +0200

    Add move AppleTree

--- ApplePicker/Assets/Scripts/AppleTree.cs
+++ ApplePicker/Assets/Scripts/AppleTree.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    //Ігровий обєкт який переміщається
+    public GameObject applePrefab;
+    //Швидкість переміщення
+    public float speed = 1f;
+    // Відстань, яку буде проходить яблуня
+    public float leftAndRightEdge = 10f;
+    // частота зміни яблук
+    public float chanceToChangeDirections = 0.1f;
+    // 1 раз у секунду буде падать яблука
+    public float secondsBetweenAppleDrops = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Зброс яблук 1 раз у сенуду
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //постійне переміщення
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
     }
 }
