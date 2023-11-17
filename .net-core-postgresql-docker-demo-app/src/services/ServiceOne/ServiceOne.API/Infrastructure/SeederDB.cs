6570ff6bb5353480c84056756a2539f03b7554d1
novakvova <novakvova@gmail>
2023-11-17 21:00:23 +0200

    Working app

---
+++ .net-core-postgresql-docker-demo-app/src/services/ServiceOne/ServiceOne.API/Infrastructure/SeederDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectManagement.Infrastructure;
+using System;
+
+namespace ServiceOne.API.Infrastructure
+{
+    public static class SeederDB
+    {
+        public static void SeedData(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices
+                    .GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDataContext>();
+                context.Database.Migrate();
+            }
+        }
+    }
+}
---
+++ .net-core-postgresql-docker-demo-app/src/services/ServiceOne/ServiceOne.API/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using ProjectManagement.Infrastructure;
+using ServiceOne.API.Infrastructure;
+using ServiceOne.API.Infrastructure.Interfaces;
+using ServiceOne.API.Infrastructure.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//var connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+Console.WriteLine(builder.Configuration["ConnectionString"]);
+var connectionString = builder.Configuration["ConnectionString"];
+
+builder.Services.AddDbContext<ApplicationDataContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.SeedData();
+
+app.Run();
