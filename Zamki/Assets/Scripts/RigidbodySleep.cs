dbb45f37d89bd8cfbc63f76126670dba44120943
novakvova <novakvova@gmail>
2023-11-27 19:46:01 +0200

    Begin wokring complete castle

---
+++ Zamki/Assets/Scripts/RigidbodySleep.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodySleep : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if(rb != null)
+            rb.Sleep();
+    }
+
+}
